4b7508471252c8ca115ac32601764022ebd81951
joshua-staples <77302413+joshua-staples@users.noreply.github.com>
2021-03-02 19:02:12 -0700

    Set backgorund color

--- project_template/battleship/__main__.py
+++ project_template/battleship/__main__.py
@@ -4,6 +4,12 @@ import constants
 
 def main():
     arcade.open_window(constants.LENGTH, constants.WIDTH, constants.SCREENNAME)
+    arcade.set_background_color(arcade.csscolor.AQUA)
+    
+    arcade.start_render()
+
+    arcade.finish_render()
+    
     arcade.run()
 
 main()
