0c23d941f01f0244f584e327e2150e3605f8d620
Spencer Wigren <spencer.wigren@outlool.com>
2021-03-05 15:51:44 -0700

    Updated Board

--- project_template/battleship/Board.py
+++ project_template/battleship/Board.py
@@ -1,50 +1,55 @@
+import ship
+
 # import constants
 import arcade
 
 #SPRITE_SCALING = 0.5
 
-# SCREEN_HEIGHT = 600
-# SCREEN_WIDTH =  600
-# SCREEN_TITLE = "Battleship"
-# FPS = 60
+SCREEN_HEIGHT = 600
+SCREEN_WIDTH =  600
+SCREEN_TITLE = "Battleship"
+FPS = 60
 
-class Board(arcade.Sprite):
+class Board(arcade.Window):
     
 
     def __init__(self):
 
-        # super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
         self.ship_list = None
         self.wave_background = None
         self.explosion_list = None
         
+        
         arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
-
-    def setup(self):
         self.ship_list = arcade.SpriteList()
-        
+                
         self.explosion_list = arcade.SpriteList()
-        
+                
         self.wave_background = arcade.load_texture(":resources:assets/water-wave_1f30a.png")
+        arcade.draw_lrwh_rectangle_textured(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, self.wave_background)
+    
+    def setup(self):
+        self.ship = ship
         
         
         # self.wave_list = arcade.load_texture(":resources:assets/water-wave_1f30a.png")
         # Will need to take this out for final project
-    # def on_draw(self):
+    def on_draw(self):
 
-    #     arcade.start_render()
-    #     self.ship_list.draw()
-    #     # self.wave_list.draw()
-    #     self.explosion_list.draw()
-    #     arcade.draw_lrwh_rectangle_textured(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, self.wave_background)
+        arcade.start_render()
+        self.ship_list.draw()
+        # self.wave_list.draw()
+        self.explosion_list.draw()
+        
 
-# def main():
-#     """ Main method """
-#     window = Board()
-#     window.setup()
-#     arcade.run()
+def main():
+    """ Main method """
+    window = Board()
+    window.setup()
+    arcade.run()
 
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
