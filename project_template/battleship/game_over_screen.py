6e0c986a09e66109c3fd12311abc7d35076ad6ac
joshua-staples <77302413+joshua-staples@users.noreply.github.com>
2021-03-27 22:31:05 -0600

    Fixed only escape to exit. Changed start color.

--- project_template/battleship/game_over_screen.py
+++ project_template/battleship/game_over_screen.py
@@ -1,21 +1,25 @@
-from arcade.key import ENTER
+from arcade.key import ESCAPE
 import constants
 import arcade
 
-class Game_Over_Screen(arcade.View, arcade.Window):
+class Game_Over_Screen(arcade.View):
     """ This handles showing the Game Over screen when the player loses or ends the game.
 
     Code gotten from the "view instruction and game over" example on 
     the arcade website.
-    Editor: Logan Huston
+    Editor: Logan Huston and Joshua Staples
     """
 
+    def __init__(self):
+        super().__init__()
+        arcade.set_viewport(0, constants.SCREEN_WIDTH - 1, 0, constants.SCREEN_HEIGHT - 1)
+        
     def on_show(self):
         """Change the back ground color.
 
             This def may not work. Check for finaly release.
         """
-        arcade.set_background_color(arcade.color.BLACK)
+        arcade.set_background_color(arcade.csscolor.BLACK)
 
     def on_draw(self):
         """ Draw the Game Over screen.
@@ -23,12 +27,15 @@ class Game_Over_Screen(arcade.View, arcade.Window):
         arcade.draw_text("Game Over", constants.SCREEN_WIDTH/2, constants.SCREEN_HEIGHT/2,
                          arcade.color.WHITE, font_size=50, anchor_x="center")
         # Need to change this and the next method to the escape key.
-        arcade.draw_text("Press ESC to exit", constants.SCREEN_WIDTH/2, constants.SCREEN_HEIGHT/2-75,
+        arcade.draw_text("Press ESC to Exit", constants.SCREEN_WIDTH/2, constants.SCREEN_HEIGHT/2-75,
                          arcade.color.WHITE, font_size=30, anchor_x="center")
+        # arcade.draw_text("Click Left Mouse to Play Again", constants.SCREEN_WIDTH/2, constants.SCREEN_HEIGHT/2-150,
+        #                  arcade.color.WHITE, font_size=30, anchor_x="center")
 
     def on_key_press(self, key, _modifiers):
         """ Use a key press to end the game.
         """
-        if arcade.key.ESCAPE:
+        if key == arcade.key.ESCAPE:
             arcade.close_window()
+
 
