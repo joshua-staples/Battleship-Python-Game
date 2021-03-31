363ef4a3612f0c98ddbf9bf7f7649be7d0176405
Spencer Wigren <spencer.wigren@outlool.com>
2021-03-31 08:31:26 -0600

    First round of cleaned up code. Socre is done

--- project_template/battleship/score.py
+++ project_template/battleship/score.py
@@ -19,8 +19,6 @@ class Score(arcade.Sprite):
         
         Args:
             score = init the score
-            enemy = class of Enemy()
-            player = class of Player()
         """
         # init the score
         self.score = 0
@@ -30,7 +28,7 @@ class Score(arcade.Sprite):
         """Will up date the score
 
         Args:
-            None
+            score = main score counter
         """
         self.score += 10
         return self.score
@@ -40,13 +38,18 @@ class Score(arcade.Sprite):
         """Will up date the score for final enemy
 
         Args:
-            None
+            score = finaly score counter
         """
         self.score += 50
         return self.score
         
     
     def get_score(self):
+        """Will show the score
+
+        Args:
+            score = score
+        """
         return self.score
         
         
@@ -59,20 +62,3 @@ class Score(arcade.Sprite):
         self.score_output = f"Score: {self.score}"
         arcade.draw_text(self.score_output, 0, constants.SCREEN_HEIGHT - 56,
                          arcade.color.GOLDENROD, 50)
-
-        # Tried colors
-        # Had to use .color, or .csscolor
-            # Liked:
-                # AQUAMARINE, RED, GOLD, GOLDEN_POPPY
-            
-            # Maybe:
-                # ROMAN_SILVER,
-
-            # Didn't Liked:
-                # SEA_GREEN, GRAY_BLUE
-
-            # GOLD_FUSION		 
-            # GOLDEN_BROWN		 
-            # GOLDEN_POPPY		 
-            # GOLDEN_YELLOW		 
-            # GOLDENROD
